4006e7696601f88f3e26568014c5eb8f70505d95
Rishabh Ramsisaria <rishabhramsisaria25@gmail.com>
2019-07-11 16:32:04 -0700

    Build A Bot

---
+++ src/router/index.js
@@ -0,0 +1,62 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HomePage from '../home/HomePage.vue';
+import RobotBuilder from '../build/RobotBuilder.vue';
+import PartInfo from '../parts/PartInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotBases from '../parts/RobotBases.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import SidebarStandard from '../sidebars/SidebarStandard.vue';
+import SidebarBuild from '../sidebars/SidebarBuild.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'Home',
+    components: {
+      default: HomePage,
+      sidebar: SidebarStandard,
+    },
+  }, {
+    path: '/build',
+    name: 'Build',
+    components: {
+      default: RobotBuilder,
+      sidebar: SidebarBuild,
+    },
+  }, {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    components: {
+      default: BrowseParts,
+      sidebar: SidebarStandard,
+    },
+    children: [{
+      name: 'BrowseHeads',
+      path: 'heads',
+      component: RobotHeads,
+    }, {
+      name: 'BrowseArms',
+      path: 'arms',
+      component: RobotArms,
+    }, {
+      name: 'BrowseBases',
+      path: 'bases',
+      component: RobotBases,
+    }, {
+      name: 'BrowseTorsos',
+      path: 'torsos',
+      component: RobotTorsos,
+    }],
+  }, {
+    path: '/parts/:partType/:id',
+    name: 'Parts',
+    component: PartInfo,
+    props: true,
+  }],
+});
